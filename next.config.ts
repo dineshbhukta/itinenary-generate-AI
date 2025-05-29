9fac189fc3d6df916fcb5285f3a98d1bdd723fd0
Dinesh Dipti Ranjan Bhukta <bhuktadinesh19@gmail.com>
2025-05-29 22:47:41 +0530

    `Update ESLint configuration and Next.js configuration to disable specific rules and type checking during build.`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // 🚨 Disables type checking during the build
+  },
 };
 
 export default nextConfig;
